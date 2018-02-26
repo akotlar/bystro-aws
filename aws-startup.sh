bd5676b46918cb754fd433f6345f6d19f93a7593
Alex <akotlar@bu.edu>
2018-02-26 18:03:58 -0500

    Update aws-startup.sh

--- aws-startup.sh
+++ aws-startup.sh
@@ -2,24 +2,24 @@
 cdir=$(pwd);
 line=$(lsblk | grep nvme | wc -l);
 
-sudo yum install gcc -y;
-sudo yum install cpan -y;
-sudo yum install openssl -y;
-sudo yum install openssl-devel -y;
+sudo yum install gcc -y -q;
+sudo yum install cpan -y -q;
+sudo yum install openssl -y -q;
+sudo yum install openssl-devel -y -q;
 # Not strictly necessary, useful however for much of what we do
-sudo yum install git-all -y;
+sudo yum install git-all -y -q;
 # pigz for Bystro, used to speed up decompression primarily
-sudo yum install pigz -y;
-sudo yum install unzip -y;
-sudo yum install wget -y;
+sudo yum install pigz -y -q;
+sudo yum install unzip -y -q;
+sudo yum install wget -y -q;
 # For tests involving querying ucsc directly
-sudo yum install mysql-devel -y;
+sudo yum install mysql-devel -y -q;
 
 # for perlbrew, in case you want to install a different perl version
 #https://www.digitalocean.com/community/tutorials/how-to-install-perlbrew-and-manage-multiple-versions-of-perl-5-on-centos-7
 # centos 7 doesn't include bzip2
-sudo yum install bzip2  -y;
-sudo yum install patch -y;
+sudo yum install bzip2  -y -q;
+sudo yum install patch -y -q;
 
 rm -rf bystro;
 git clone git://github.com/akotlar/bystro.git
